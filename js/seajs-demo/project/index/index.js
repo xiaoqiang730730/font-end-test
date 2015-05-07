9aceb18c2b5e5e6a0bba8dcf032e885c47c1a359
yanjunqiang <yanjunqiang@topthink.net>
2015-05-07 11:25:20 +0800

    seajs

--- js/seajs-demo/project/index/index.js
+++ js/seajs-demo/project/index/index.js
@@ -1,22 +1,18 @@
-define('project/index/index',function(require,exports,module){
+define('project/index/index', function(require, exports, module) {
 	// require('component/jquery/dist/jquery');
 	require('component/fullpage.js/vendors/jquery.slimscroll.min');
-    require('component/fullpage.js/jquery.fullPage');
-	//   Class({
-	//     initial: function(){
-	//         var _page        = $('.m-page');
-	//         var _pageAnchors = ['one', 'two', 'three','four','five','six','seven'];
-	//         $('#fullpage').fullpage({
-	//             anchors: _pageAnchors,
-	//             navigation: true,
-	//             navigationPosition: 'right',
-	//             onLeave: function(leavingSection, leavingSectionIndex, sectionIndex, yMovement){
-	//                 $.each(_pageAnchors, function(i,v){
-	//                     _page.removeClass('page-fullpage-'+v);
-	//                 });
-	//                 _page.addClass('page-fullpage-'+_pageAnchors[leavingSectionIndex-1]);
-	//             }
-	//         });
-	//     }
-	// });
+	require('component/fullpage.js/jquery.fullPage');
+	var _page = $('.m-page');
+	var _pageAnchors = ['one', 'two', 'three', 'four', 'five', 'six', 'seven'];
+	$('#fullpage').fullpage({
+		anchors: _pageAnchors,
+		navigation: true,
+		navigationPosition: 'right',
+		onLeave: function(leavingSection, leavingSectionIndex, sectionIndex, yMovement) {
+			$.each(_pageAnchors, function(i, v) {
+				_page.removeClass('page-fullpage-' + v);
+			});
+			_page.addClass('page-fullpage-' + _pageAnchors[leavingSectionIndex - 1]);
+		}
+	});
 });
